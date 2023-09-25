260510c791d173e6f95d7b194e88e2fb65c3765b
pritamsahu99 <pritamsahuofficial@gmail.com>
2023-09-26 00:06:46 +0530

    update

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ TxtType.prototype.tick = function() {
     that.tick();
     }, delta);
 };
-window.onload = function() {
+window.addEventListener("load", function () {
     var elements = document.getElementsByClassName('typewrite');
     for (var i=0; i<elements.length; i++) {
         var toRotate = elements[i].getAttribute('data-type');
@@ -48,9 +48,49 @@ window.onload = function() {
     }
     // INJECT CSS 
     var css = document.createElement("style");
-    css.t = "text/css";
+    css.text = "text/css";
     document.body.appendChild(css);
-};
+});
+
+//-------------------CodeHelp Blur Loader------------------------------------------
+
+const loader = document.getElementById("blur-container");
+const wrapper = document.getElementById("container");
+
+function animateEffects() {
+  let scaleValue = 0; // Initial scale value
+
+  const animationInterval = setInterval(() => {
+    scaleValue += 0.02; // Adjust the increment for a smoother transition
+    loader.style.transform = `scale(${scaleValue})`;
+
+    if (scaleValue >= 1) {
+      clearInterval(animationInterval);
+      setTimeout(zoomInImage, 1500); // Call the zoomInImage function after a delay
+    }
+  }, 25); // Adjust the interval for smoother animation
+}
+
+function zoomInImage() {
+  let blurAmount = 0; 
+  let scaleValue = 1; 
+
+  const zoomInterval = setInterval(() => {
+    blurAmount += 1;
+    scaleValue += 0.1;
+    loader.style.filter = `blur(${blurAmount}px)`;
+    loader.style.transform = `scale(${scaleValue})`;
+  
+    if (blurAmount >= 20) {
+      clearInterval(zoomInterval);
+      loader.style.display = "none";
+      loader.style.opacity = 0;
+      wrapper.style.display = "block";
+    }
+  }, 60); // Adjust the interval for smoother animation
+}
+window.addEventListener("load", animateEffects);
+
 //-------------Animations Takes Place Here----------------------------------------------------
 
 const sections = document.querySelectorAll('section');
@@ -72,3 +112,16 @@ window.onscroll = () => {
   }     
   prevScrollY = currentScrollY; // Update the previous scroll position
 };
+
+//-------------------CodeHelp ToggleDark Mode------------------------------------------
+document.getElementById('svg1').addEventListener('click', function() {
+  // Toggle visibility of the SVGs
+  document.getElementById('svg1').style.display = 'none';
+  document.getElementById('svg2').style.display = 'block';
+});
+// Add a click event listener to the second SVG
+document.getElementById('svg2').addEventListener('click', function() {
+  // Toggle visibility of the SVGs
+  document.getElementById('svg2').style.display = 'none';
+  document.getElementById('svg1').style.display = 'block';
+});
